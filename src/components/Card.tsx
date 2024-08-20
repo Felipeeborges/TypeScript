a00a820aa9843a2db743bfe17c4c73caf5a93594
Felipeeborges <felipe.ustrc@gmail.com>
2024-08-20 18:08:50 -0300

    Operador ternario JS

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,24 @@
-
-interface CardProps{
-    produto:string
-    valor:number
-    desconto:number
+// Definição da interface para as props do componente Card
+interface CardProps {
+    produto: string
+    valor: number
+    desconto: number
+    funcao: any // Função para calcular o preço com desconto
 }
 
-export default function Card(props:CardProps){
+// Definição do componente Card
+export default function Card(props: CardProps) {
     return (
+        // Div que estiliza o Card com classes Tailwind CSS
         <div className='flex flex-col border border-red-700 rounded-sm p-1'>
             <div>Produto: {props.produto}</div>
             <div>Valor: {props.valor}</div>
-            <div>Desconto: {props.desconto}</div>
-            <div>Preço venda: {props.valor - props.desconto}</div>
+            {props.desconto>0?(  // Operação Ternária
+                <div>
+                    <div>Desconto: {props.desconto}</div>
+                    <div>Preço venda: {props.funcao(props.valor, props.desconto)}</div>
+                </div>
+            ):"Sem desconto"}
         </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,52 @@
-// Index Landing Page
-
+// Importando os componentes Topo e Card
 import Topo from "@/components/Topo"
 import Card from "@/components/Card"
 
-const nome = 'Felaaaaaaas'
+// Declarando uma constante e uma variável com valores de nome e canal
+const nome = 'Felipe Borges'
 let canal = 'Curso Type Script'
 
+// Função que retorna o valor da constante 'nome'
 function retnome() {
   return nome
 }
 
+// Função que calcula o valor com desconto subtraindo o desconto do valor original
+function calcDesc(v:number, d:number) {
+  return v - d
+}
 
+  // Função que calcula o valor com desconto subtraindo o desconto do valor original
+function calcDesc2(v:number, d:number) {
+  return v - (d/2)
+}
+
+// Componente principal da página inicial
 export default function Home() {
   return (
     <div>
       <Topo/>
       <div style={stilo}>
-        <div>{retnome()}</div>
+        <div>{retnome()}</div> 
         <div>{canal}</div>
       </div>
+
       <div className='flex justify-center gap-3'>
-      <Card produto={'Mouse'} valor ={50.00} desconto={0.2}/>
-      <Card produto={'Teclado'} valor ={10.00} desconto={0.5}/>
-      <Card produto={'Tecla'} valor ={5.00} desconto={0.9}/>  
-      <Card produto={'CPU'} valor ={795.00} desconto={10.2}/>    
-      </div>
+
+        <Card produto={'Mouse'} valor={50.00} desconto={0.2} funcao={calcDesc} />
+        <Card produto={'Teclado'} valor={10.00} desconto={0.5} funcao={calcDesc} />
+        <Card produto={'Tecla'} valor={5.00} desconto={0.9} funcao={calcDesc} />  
+        <Card produto={'CPU'} valor={795.00} desconto={10.2} funcao={calcDesc} />
+        <Card produto={'Monitor'} valor={480.00} desconto={0.7} funcao={calcDesc} />
+        <Card produto={'Mouse Pad'} valor={45.00} desconto={0} funcao={calcDesc} />
+        <Card produto={'Iphone'} valor={5000.00} desconto={8.9} funcao={calcDesc2} />  
+        <Card produto={'Celular'} valor={4512.00} desconto={12.2} funcao={calcDesc2} />    
+      </div>    
     </div>
-  );
+  )// Componente principal da página inicial
 }
 
+// Objeto de estilo para a div que contém nome e canal
 const stilo = {
   display: 'flex',
   justifyContent: 'center',
@@ -36,5 +54,5 @@ const stilo = {
   color: '#00f',
   backgroundColor: '#eee',
   fontSize: '40px',
-  gap: '500px'  // Adiciona espaçamento horizontal de 20px entre os elementos
+  gap: '500px'  // Adiciona espaçamento horizontal de 500px entre os elementos
 }
